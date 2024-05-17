2643ee8a1a9e12a7958a94fcf8bb5fba26b1b8d1
PhucPQ_00 <phucpq07122000@gmail.com>
2024-05-17 22:46:05 +0700

    get soter

--- TAT.StoreLocator.Core/Models/Response/Store/StoreResponseModel.cs
+++ TAT.StoreLocator.Core/Models/Response/Store/StoreResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TAT.StoreLocator.Core.Models.Response.Store
+﻿namespace TAT.StoreLocator.Core.Models.Response.Store
 {
     public class StoreResponseModel
     {
@@ -12,13 +6,12 @@ namespace TAT.StoreLocator.Core.Models.Response.Store
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public string? RoadName { get; set; }
-        public string? Province { get; set; }
-        public string? District { get; set; }
-        public string? Ward { get; set; }
-        public string? PostalCode { get; set; }
-        public decimal latitude { get; set; }
-        public decimal longtitude { get; set; }
+        public AddressResponseModel? Address { get; set; }
+        public DateTimeOffset? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+
         public List<MapGalleryStoreResponse> MapGalleryStores { get; set; } = new List<MapGalleryStoreResponse>();
     }
 
@@ -28,5 +21,22 @@ namespace TAT.StoreLocator.Core.Models.Response.Store
         public string? FileName { get; set; }
         public string? Url { get; set; }
         public bool IsThumbnail { get; set; }
+    }
+
+    public class AddressResponseModel
+    {
+        public string? RoadName { get; set; }
+        public string? Province { get; set; }
+        public string? District { get; set; }
+        public string? Ward { get; set; }
+        public string? PostalCode { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+
+    public class RatingStore
+    {
+        public int NumberRating { get; set; }
+        public double PointOfRating { get; set; }
     }
 }
