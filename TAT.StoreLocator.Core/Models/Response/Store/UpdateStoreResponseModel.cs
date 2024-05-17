a9da3d1b4f4d5895d155883c8e84a00a1ee0268a
phucthinh2610 <bimapmap1234567@gmail.com>
2024-05-17 08:02:46 +0700

    add UpdateStore

---
+++ TAT.StoreLocator.Core/Models/Response/Store/UpdateStoreResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAT.StoreLocator.Core.Common;
+
+namespace TAT.StoreLocator.Core.Models.Response.Store
+{
+    public class UpdateStoreResponseModel
+    {
+        public BaseResponse BaseResponse { get; set; } = new BaseResponse();
+        public StoreResponseModel StoreResponseModel { get; set; }  = new StoreResponseModel();
+    }
+}
