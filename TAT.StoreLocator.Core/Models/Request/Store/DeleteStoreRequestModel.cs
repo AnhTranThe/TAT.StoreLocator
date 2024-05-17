9be6eaca2a8574eb275eef8d72b95bbe519c6783
phucthinh2610 <bimapmap1234567@gmail.com>
2024-05-17 07:37:37 +0700

    add DeleteStore

---
+++ TAT.StoreLocator.Core/Models/Request/Store/DeleteStoreRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAT.StoreLocator.Core.Models.Request.Store
+{
+    public class DeleteStoreRequestModel
+    {
+        public string? Id { get; set; } 
+    }
+}
