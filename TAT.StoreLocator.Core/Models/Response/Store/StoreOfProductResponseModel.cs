c288db5704c1bfd8896f85085d81a23d6033986b
yuhpham <91568570+yuhpham@users.noreply.github.com>
2024-05-18 22:50:04 +0700

    getproductbyid getlistproduct addproduct updateproduct

---
+++ TAT.StoreLocator.Core/Models/Response/Store/StoreOfProductResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TAT.StoreLocator.Core.Models.Response.Store
+{
+    public class StoreOfProductResponseModel
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
