cc24ff41f253b97c06d79670ae88ebccf2e344d0
phucthinh2610 <bimapmap1234567@gmail.com>
2024-05-18 08:21:15 +0700

    add CreateReview

---
+++ TAT.StoreLocator.Core/Interface/IServices/IReviewService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAT.StoreLocator.Core.Common;
+using TAT.StoreLocator.Core.Models.Request.Review;
+using TAT.StoreLocator.Core.Models.Response.Review;
+
+namespace TAT.StoreLocator.Core.Interface.IServices
+{
+   public interface IReviewService
+    {
+        // Create
+        Task<BaseResponseResult<ReviewResponseModel>> CreateReviewAsync(CreateReviewRequestModel request);
+    }
+}
