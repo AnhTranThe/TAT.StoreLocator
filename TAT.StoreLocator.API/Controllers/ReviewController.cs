76dd39b7eb3376238022c54e415c4a299ce3d265
phucthinh2610 <bimapmap1234567@gmail.com>
2024-05-18 08:20:57 +0700

    add CreateReview

---
+++ TAT.StoreLocator.API/Controllers/ReviewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TAT.StoreLocator.Core.Models.Request.Review;
+using TAT.StoreLocator.Core.Interface.IServices;
+
+namespace TAT.StoreLocator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewService _reviewService;
+       
+        public ReviewController (IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        [HttpPost("create")]
+        public  async Task<IActionResult> CreateReview([FromBody]CreateReviewRequestModel request)
+        {
+            var response = await _reviewService.CreateReviewAsync(request);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+
+
+    }
+}
