d5e418446f7cd8a822dad0d47d316721ac3eec0f
trần thế anh <trantheanh132@gmail.com>
2024-05-21 11:50:39 +0700

    -- update category controller with get parent , sub -- fix refresh-token can not refresh when token expired -- add new generate Token and refresh token service

--- TAT.StoreLocator.Core/Models/Token/Request/RefreshTokenRequest.cs
+++ TAT.StoreLocator.Core/Models/Token/Request/RefreshTokenRequest.cs
@@ -2,7 +2,7 @@
 {
     public class RefreshTokenRequest
     {
-        public string? AccessToken { get; set; }
-        public string? RefreshToken { get; set; }
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
     }
 }
